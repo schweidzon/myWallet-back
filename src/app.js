633e6a4e4dd204c33ebe03f459a996a1575d7d66
Schweidzon <dannielsch@gmail.com>
2023-01-18 20:32:42 -0300

    Adicionando rota de GET/values para pegar os valores da carteira do usuario e o de POST/update-wallet para adicionar valores na carteira

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ import cors from 'cors'
 import joi from 'joi'
 import { MongoClient, ObjectId } from 'mongodb'
 import dotenv from 'dotenv'
-import {registerUserSchema} from '../validators.js'
+import { registerUserSchema } from '../validators.js'
+import dayjs from 'dayjs'
+import bcrypt from 'bcrypt'
 dotenv.config()
 
 const app = express()
@@ -17,31 +19,72 @@ try {
 
     await mongoClient.connect()
     db = mongoClient.db()
-    
+
 } catch (erro) {
     console.log(erro)
-       
+
 }
 
-app.post("/cadastro", (req, res) => {
+app.post("/cadastro", async (req, res) => {
     const user = req.body
-    const validateUser= registerUserSchema.validate(user, {abortEarly:false})
-   
+    console.log(user)
+    const validateUser = registerUserSchema.validate(user, { abortEarly: false })
+
+    const encryptPassword = bcrypt.hashSync(user.password, 10)
+
+    const checkUserExist = await db.collection("users").findOne({ name: user.name })
+    console.log(checkUserExist)
+    if (checkUserExist) return res.status(409).send("Usuário já cadastrado!")
+
 
     if (validateUser.error) {
         const erros = validateUser.error.details.map((err) => {
-            if(err.message ==='"confirmPassword" must be [ref:password]' ) {
+            if (err.message === '"confirmPassword" must be [ref:password]') {
                 err.message = "confirmPassword tem que ser igual ao password"
             }
-            
+            if (err.message.includes("/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]+$/")) {
+                err.message = "Nome de usuário inválido"
+            }
+
             return err.message
         })
         return res.status(422).send(erros)
     }
-    db.collection("user").insertOne({user})
-    res.status(201).send("Usuário criado com sucesso")
-  
-}) 
+    await db.collection("users").insertOne({...req.body, password:encryptPassword,wallet: []})
+    res.status(201).send("Usuário cadastrado com sucesso")
+
+})
+
+app.post("/", async (req, res) => {
+    const user = req.body
+    console.log(user.email)
+    console.log(user.password)
+    //console.log( await db.collection("users").find().toArray())
+    const userExist = await db.collection("users").findOne({ email: user.email })
+
+    if (!userExist) return res.status(400).send("Usuário ou senha incorretos")
+
+    const matchPassword = bcrypt.compareSync(user.password, userExist.password )
+
+    if(!matchPassword) return res.status(400).send("Usuário ou senha incorretos")
+
+    return res.send(userExist)
+})
+
+app.get("/values", async(req, res) => {   
+    const user = req.headers.user  
+    const userExist = await db.collection("users").findOne({name:user})   
+    return res.send(userExist)
+})
+
+app.post("/update-wallet", async (req, res) => {
+    const newValue = req.body
+    await db.collection("users").updateOne({name: newValue.user}, {$push: { wallet: {value: newValue.value, description: newValue.description, type: newValue.type, date: dayjs().format("DD/MM") }}})
+    //console.log( await db.collection("users").findOne({name: newValue.user}) )
+    return res.send("ok")
+})
+
+
 
 const PORT = 5000
 
--- validators.js
+++ validators.js
@@ -1,9 +1,9 @@
 import joi from 'joi'
 
 export const registerUserSchema = joi.object( {
-    name: joi.string().required(),
+    name: joi.string().pattern(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]+$/).required(),
     email: joi.string().email().required(),
-    password: joi.string().required(),
+    password: joi.string().min(6).required(),
     confirmPassword: joi.any().valid(joi.ref('password')).required()
 
 }).allow("name", "email", "password", "confirmPassword")
